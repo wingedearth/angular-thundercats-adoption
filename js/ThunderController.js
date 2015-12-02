5a1630b682538e3686f4f14451b263d11a390338
Andrew Anissi <andrew@wingedearth.com>
2015-12-01 22:53:00 -0800

    Umm...whoops, I kinda completed everything. Second commit? Well, to be specific I created two columns and used Angular to shift Thundercats back and forth between the columns with buttons that alter their 'adopted' boolean. Thundercats...Ho

--- js/ThunderController.js
+++ js/ThunderController.js
@@ -14,46 +14,63 @@
     vm.thundercats = [
 			{
 				catName: 'Liono',
-				yearsOld: 25,
+				yearsOld: '25',
 				gender: 'male',
 				image: "images/liono.jpg",
 				adopted: true,
 			},
 			{
 				catName: 'Panthro',
-				yearsOld: 42,
+				yearsOld: '42',
 				gender: 'male',
 				image: "images/panthro.jpg",
 				adopted: false
 			},
 			{
 				catName: 'Cheetara',
-				yearsOld: 22,
+				yearsOld: '22',
 				gender: 'female',
 				image: "images/cheetara.jpg",
 				adopted: false
 			},
 			{
 				catName: 'Snarf',
-				yearsOld: 17,
+				yearsOld: '17',
 				gender: 'male',
 				image: "images/snarf.jpg",
 				adopted: false
 			},
 			{
 				catName: 'WilyKit',
-				yearsOld: 11,
+				yearsOld: '11',
 				gender: 'female',
 				image: "images/wilykit.jpg",
 				adopted: false
 			},
 			{
 				catName: 'WilyKat',
-				yearsOld: 12,
+				yearsOld: '12',
 				gender: 'male',
 				image: "images/wilykat.jpg",
 				adopted: false
 			}
 		];
+
+		vm.notAdopted = notAdopted;
+		vm.adopted = adopted;
+
+		function notAdopted() {
+			return vm.thundercats.filter(function(cat) {
+				return !cat.adopted;
+      });
+		}
+
+		function adopted() {
+			return vm.thundercats.filter(function(cat) {
+				return cat.adopted;
+      });
+		}
+
 	}
+
 })();
