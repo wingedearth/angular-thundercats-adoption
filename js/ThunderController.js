e4a1919252b55ab57fdc389d1bdbf956d27183b1
Andrew Anissi <andrew@wingedearth.com>
2015-12-02 14:29:54 -0800

    make 'add cat' button and form functional.

--- js/ThunderController.js
+++ js/ThunderController.js
@@ -56,9 +56,18 @@
 			}
 		];
 
+		// assign function variables for export via controller
 		vm.notAdopted = notAdopted;
-		vm.adopted = adopted;
-		vm.deleteCat = deleteCat;
+		vm.adopted 		= adopted;
+		vm.deleteCat 	= deleteCat;
+		vm.addCat 		= addCat;
+		vm.newCat 		= {
+			catName  : "",
+			yearsOld : "",
+			gender 	 : "",
+			image    : "",
+			adopted  : false
+		};
 
 		function notAdopted() {
 			return vm.thundercats.filter(function(cat) {
@@ -72,9 +81,20 @@
       });
 		}
 
-		// function addCat() {
-
-		// }
+		function addCat() {
+			vm.thundercats.push({
+				catName: vm.newCat.catName,
+				yearsOld: vm.newCat.yearsOld,
+				gender: vm.newCat.gender,
+				image: vm.newCat.image,
+				adopted: false
+			});
+			vm.newCat.catName 	= "";
+			vm.newCat.yearsOld 	= "";
+			vm.newCat.gender 	 	= "";
+			vm.newCat.image    	= "",
+			vm.newCat.adopted  	= false;
+		}
 
 		function deleteCat(removeCat) {
 			vm.thundercats = vm.thundercats.filter(function(cat) {
